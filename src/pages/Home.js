429b509783cd2024e4c5efb7a253c610ecba9aed
Pravin Jadhav <pr.jadhav02@gmail.com>
2021-12-30 16:20:10 +0530

    update

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import AppsIcon from '@material-ui/icons/Apps';
+import Avatar from '@material-ui/core/Avatar';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__header">
+            <div className="home__headerLeft">
+            <p>About</p>
+            <p>Store</p>
+            </div>
+            <div className="home__headerRight">
+            <p>Gmail</p>
+            <p>Images</p>
+            <AppsIcon/>
+            <Avatar/>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
